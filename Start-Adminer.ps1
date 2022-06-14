37e63f58f51b195cfb9cebade24b29649635bc54
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-06-14 14:18:31 +0200

    no more zelfstudie :D

--- Start-Adminer.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Kaartje app server is running"    
-docker run `
-    --name kaartje-db-admin-server `
-    --publish 9999:8080 `
-    --detach adminer:latest
--- Start-Postgresql.ps1
+++
@@ -1,9 +0,0 @@
-docker run `
-    --name kaartje-db-server `
-    --env POSTGRES_USER=intec `
-    --env POSTGRES_PASSWORD=intec `
-    --env POSTGRES_DB=kaartje `
-    --volume ./db:/var/lib/postgresql/data `
-    --publish 5432:5432 `
-    --rm `
-    --detach postgres:latest
