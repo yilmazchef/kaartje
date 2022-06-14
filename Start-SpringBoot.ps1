a6a9a2b4fcc36976116f0cfc627c19d1ac736f62
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-06-14 13:31:49 +0200

    dockers

--- Start-SpringBoot.ps1
+++ Start-SpringBoot.ps1
@@ -1,11 +1,11 @@
-docker build . -t yilmazchef/kaartje:latest
+docker build -t yilmazchef/kaartje:latest .
 docker push yilmazchef/kaartje:latest
 docker run `
     --name kaartje-app-server `
+    --rm `
     --env DB_HOST=kaartje-db-server `
     --env DB_PORT=8080 `
     --env DB_NAME=kaartje `
     --env DB_USER=intec `
     --env DB_PASSWORD=intec `
-    --rm `
     --detach yilmazchef/kaartje:latest
