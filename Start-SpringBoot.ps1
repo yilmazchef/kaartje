8159f66729a4b3f3d3a3124070ee2d0e1dd29f59
Yilmaz MUSTAFA <yilmaz.mustafa@intecbrussel.be>
2022-06-14 13:58:10 +0200

    run spring boot

--- Start-SpringBoot.ps1
+++ Start-SpringBoot.ps1
@@ -1,11 +1,13 @@
+Write-Host "Kaartje is being built..."
 docker build -t yilmazchef/kaartje:latest .
-docker push yilmazchef/kaartje:latest
+Write-Host "Kaartje app server is running"    
 docker run `
     --name kaartje-app-server `
-    --rm `
-    --env DB_HOST=kaartje-db-server `
+    --env DB_HOST=localhost `
     --env DB_PORT=5432 `
     --env DB_NAME=kaartje `
     --env DB_USER=intec `
     --env DB_PASSWORD=intec `
     --detach yilmazchef/kaartje:latest
+Write-Host "Pushing to Docker Hub"
+docker push yilmazchef/kaartje:latest
