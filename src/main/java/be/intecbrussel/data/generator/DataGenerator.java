dd9a2a3ce5aea634b0a681d5d4e2a4ef04e39b85
Yilmaz Mustafa <yilmaz_mustafa@outlook.be>
2022-06-18 22:23:18 +0200

    faker library is added to generate more randomized data.

--- src/main/java/be/intecbrussel/data/generator/DataGenerator.java
+++ src/main/java/be/intecbrussel/data/generator/DataGenerator.java
@@ -6,57 +6,65 @@ import be.intecbrussel.data.service.DepartmentRepository;
 import be.intecbrussel.data.service.ResponseRepository;
 import be.intecbrussel.data.service.TicketRepository;
 import be.intecbrussel.data.service.UserRepository;
+import com.github.javafaker.Faker;
 import com.vaadin.flow.spring.annotation.SpringComponent;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.text.MessageFormat;
 import java.util.Collections;
+import java.util.Locale;
 import java.util.Set;
 
 @SpringComponent
+@Slf4j
 public class DataGenerator {
 
     @Bean
     public CommandLineRunner loadData(PasswordEncoder passwordEncoder, UserRepository userRepository,
                                       TicketRepository ticketRepository, ResponseRepository resRepo,
                                       DepartmentRepository deptRepo) {
+
+        final var faker = new Faker(Locale.forLanguageTag("be-NL"));
+
         return args -> {
             Logger logger = LoggerFactory.getLogger(getClass());
             if (userRepository.count() != 0L) {
                 logger.info("Using existing database");
                 return;
             }
-            int seed = 123;
 
-            logger.info("Generating demo data");
-
-            logger.info("... generating 2 User entities...");
+            final var userPwd = faker.internet().password();
             final var user = new User()
-                    .setFirstName("John")
-                    .setLastName("Normal")
-                    .setUsername("user")
-                    .setHashedPassword(passwordEncoder.encode("user"))
+                    .setFirstName(faker.name().firstName())
+                    .setLastName(faker.name().lastName())
+                    .setUsername(faker.name().username())
+                    .setHashedPassword(passwordEncoder.encode(userPwd))
                     .setProfilePictureUrl(
                             "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80")
                     .setRoles(Collections.singleton(Role.USER));
 
-            userRepository.save(user);
+            final var savedUser = userRepository.save(user);
+            log.info(MessageFormat.format("Saved user {0} with pwd: {1} and id {2}", savedUser.getUsername(), userPwd, savedUser.getId()));
 
+            final var adminPwd = faker.internet().password();
             final var admin = new User()
-                    .setFirstName("Emma Powerful")
-                    .setUsername("admin")
-                    .setHashedPassword(passwordEncoder.encode("admin"))
+                    .setFirstName(faker.name().firstName())
+                    .setLastName(faker.name().lastName())
+                    .setUsername(faker.name().username())
+                    .setHashedPassword(passwordEncoder.encode(adminPwd))
                     .setProfilePictureUrl(
                             "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80")
                     .setRoles(Set.of(Role.USER, Role.ADMIN));
 
-            userRepository.save(admin);
+            final var savedAdmin = userRepository.save(admin);
+            log.info(MessageFormat.format("Saved admin {0} with pwd: {1} and id {2}", savedAdmin.getUsername(), adminPwd, savedAdmin.getId()));
 
-            logger.info("... generating 100 Ticket entities...");
-
+            log.info(MessageFormat.format("{0} user entities are created...", 2));
         };
     }
 
